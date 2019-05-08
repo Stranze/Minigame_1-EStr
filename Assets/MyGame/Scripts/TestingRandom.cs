cebe13f2638f48c04dc5b8d2caaa2b983dca45d0
Elisa Stranzinger <elisa.stranzinger@htl-salzburg.ac.at>
2019-05-08 15:34:21 +0200

    Testing Scene change and random generator

---
+++ Assets/MyGame/Scripts/TestingRandom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingRandom : MonoBehaviour {
+
+    public float minValX = -6f;
+    public float maxValX = 6f;
+    private float rndVal;
+    private float rndVal2;
+
+    private System.Random rndVal1 = new System.Random();
+
+    // Use this for initialization
+    void Start () {
+        rndVal = rndVal1.Next(-6, 7);
+        rndVal2 = UnityEngine.Random.Range(4, 100);
+        Debug.Log(rndVal);
+        Debug.Log(rndVal2);
+        gameObject.transform.position = new Vector3(rndVal, 0f, 0f);
+	}
+
+    public void ChangeRandomCollectable()
+    {
+        gameObject.transform.position = new Vector3(rndVal, 0f, 0f);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/MyGame/Scripts/changeTexts.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class changeTexts : MonoBehaviour {
+
+    public Text myText; 
+    public TextMeshProUGUI myTMPText;
+    public TestingRandom myScript;
+
+    private int countPresses = 0;
+
+
+    private System.Random rnd = new System.Random();
+    private System.Random seedRnd = new System.Random(123);
+
+    // Use this for initialization
+    void Start () {
+        myText.text = "Hallo";
+        myTMPText.text = "meh";
+        myText.fontSize = 30;
+        Debug.Log(rnd.Next(0, 5));
+        Debug.Log("yep " + UnityEngine.Random.Range(0, 1));
+    }
+	
+    public void addPress()
+    {
+        //machen das gleiche
+        countPresses++;
+        //countPresses = countPresses + 1;
+        //countPresses += 1;
+        Debug.Log("counts: " + countPresses);
+        myText.text = countPresses.ToString();
+        Debug.Log("seed " + seedRnd.Next(0, 5));
+        myScript.ChangeRandomCollectable();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
