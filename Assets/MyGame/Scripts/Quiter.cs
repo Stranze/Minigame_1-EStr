62feaca938abe1c5d0fc750df3a930fca3d42282
Elisa Stranzinger <elisa.stranzinger@htl-salzburg.ac.at>
2019-05-28 08:35:14 +0200

    IntroScene Script finished - SceneChanger, Quit game

---
+++ Assets/MyGame/Scripts/Quiter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiter : MonoBehaviour {
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("esxit");
+    }
+
+}
